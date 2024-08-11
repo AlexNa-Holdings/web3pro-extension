45382cf34a173f65054c58f6b78123075dbc533a
Alex Na <alexn@lexansoft.com>
2024-08-11 15:34:06 -0700

    chore: Update content.js and inject.js to relay messages between extension and page

--- src/content.js
+++ src/content.js
@@ -1,5 +1,34 @@
-// content.js
-const scriptElement = document.createElement('script');
-scriptElement.src = chrome.runtime.getURL('inject.js'); // Points to your inject.js file in the extension's directory
+// relay messages from the extension to the page and vice versa
+chrome.runtime.onMessage.addListener((payload, sender, sendResponse) =>
+  window.postMessage(
+    { type: "eth:payload", payload: payload },
+    window.location.origin
+  )
+);
+
+window.addEventListener("message", (event) => {
+  if (event.source !== window) return;
+  if (!event.data) return;
+  if (event.data.type === "eth:send")
+    chrome.runtime.sendMessage(event.data.payload); // Relay the message to the extension
+  if (event.data.type === "web3pro:get-mm") {
+    chrome.storage.sync.get("__web3proAppearAsMM__", function (result) {
+      let mmAppear = result["__web3proAppearAsMM__"];
+      try {
+        mmAppear = JSON.parse(mmAppear);
+      } catch (e) {
+        mmAppear = false;
+      }
+      window.postMessage(
+        { type: "web3pro:mm", payload: mmAppear },
+        window.location.origin
+      );
+    });
+  }
+});
+
+// Inject the script into the page
+const scriptElement = document.createElement("script");
+scriptElement.src = chrome.runtime.getURL("inject.js"); // Points to your inject.js file in the extension's directory
 scriptElement.onload = () => scriptElement.remove(); // Remove the script tag after it has been executed
 (document.head || document.documentElement).appendChild(scriptElement);
--- src/inject.js
+++ src/inject.js
@@ -1,62 +1,59 @@
-const EventEmitter = require('events')
-const EthereumProvider = require('ethereum-provider')
-
-chrome.runtime.onMessage.addListener((payload, sender, sendResponse) => window.postMessage({type: 'eth:payload', payload: payload}, window.location.origin))
-window.addEventListener('message', event => {
-  if (event.source === window && event.data && event.data.type === 'eth:send') chrome.runtime.sendMessage(event.data.payload)
-})
+const EventEmitter = require("events");
+const EthereumProvider = require("ethereum-provider");
 
 class Connection extends EventEmitter {
-  constructor () {
-    super()
-    window.addEventListener('message', event => {
-      if (event && event.source === window && event.data && event.data.type === 'eth:payload') {
-        this.emit('payload', event.data.payload)
+  constructor() {
+    super();
+    window.addEventListener("message", (event) => {
+      if (!event || !event.data || !event.data.type) return;
+      if (event.source !== window) return;
+      if (event.data.type === "eth:payload") {
+        this.emit("payload", event.data.payload);
       }
-    })
-    setTimeout(() => this.emit('connect'), 0)
+    });
+    setTimeout(() => this.emit("connect"), 0);
   }
 
-  send (payload) {
-    window.postMessage({ type: 'eth:send', payload }, window.location.origin)
+  send(payload) {
+    window.postMessage({ type: "eth:send", payload }, window.location.origin);
   }
 }
 
-chrome.storage.sync.get('__web3proAppearAsMM__', function(result) {
-  let mmAppear = result['__web3proAppearAsMM__'];
-  try {
-    mmAppear = JSON.parse(mmAppear)
-  } catch (e) {
-    mmAppear = false
-  }
+window.addEventListener("message", (event) => {
+  if (!event || !event.data || !event.data.type) return;
+  if (event.source !== window) return;
+  if (event.data.type === "web3pro:mm") {
+    console.log("appear as MM:", event.data.payload);
+    mmAppear = JSON.stringify(event.data.payload);
+    console.log("appear as MM:", mmAppear);
 
-  console.log('appear as MM:', mmAppear)
-
-  if (mmAppear) {
-    class MetaMaskProvider extends EthereumProvider { }
-    try {
-      window.ethereum = new MetaMaskProvider(new Connection())
-      window.ethereum.isMetaMask = true
-      window.ethereum._metamask = {
-        isUnlocked: () => true
+    if (mmAppear) {
+      class MetaMaskProvider extends EthereumProvider {}
+      try {
+        window.ethereum = new MetaMaskProvider(new Connection());
+        window.ethereum.isMetaMask = true;
+        window.ethereum._metamask = {
+          isUnlocked: () => true,
+        };
+        window.ethereum.setMaxListeners(0);
+        console.log("appear as MM");
+      } catch (e) {
+        console.error("Web3Pro Error:", e);
       }
-      window.ethereum.setMaxListeners(0)
-      console.log('appear as MM')
-    } catch (e) {
-      console.error('Web3Pro Error:', e)
+    } else {
+      class Web3ProProvider extends EthereumProvider {}
+      try {
+        window.ethereum = new Web3ProProvider(new Connection());
+        window.ethereum.isWeb3Pro = true;
+        window.ethereum.setMaxListeners(0);
+        console.log("appear as Web3Pro");
+      } catch (e) {
+        console.error("Web3Pro Error:", e);
+      }
     }
-  } else {
-    class Web3ProProvider extends EthereumProvider { }
-    try {
-      window.ethereum = new Web3ProProvider(new Connection())
-      window.ethereum.isWeb3Pro = true
-      window.ethereum.setMaxListeners(0)
-      console.log('appear as Web3Pro')
-    } catch (e) {
-      console.error('Web3Pro Error:', e)
-    }
-  }
 
-  console.log('window.ethereum', window.ethereum)
-  
+    console.log("window.ethereum", window.ethereum);
+  }
 });
+
+window.postMessage({ type: "web3pro:get-mm" }, window.location.origin);
